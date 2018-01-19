76feebba39dfae0d890893b81e495a7402397a0a
dodechotomy <dodechotomy@gmail.com>
2018-01-19 00:42:37 -0500

    Added twitter code

--- weirdbirds.js
+++ weirdbirds.js
@@ -1,25 +1,39 @@
 // Require libraries
 
-    const Improv = require('improv');
-    const yaml = require('js-yaml');
-    const fs = require('fs-jetpack');
+const Improv = require('improv');
+const yaml = require('js-yaml');
+const fs = require('fs-jetpack');
+// Load our data from a file
+const grammarData = yaml.load(fs.read('grammar.yaml'));
+// console.log(grammarData);
+// Create a generator object from this data
+const generator = new Improv(grammarData, {
+    filters: [Improv.filters.mismatchFilter(), Improv.filters.unmentioned(2)],
+    reincorporate: true
+});
 
-    // Load our data from a file
-    const grammarData = yaml.load(fs.read('grammar.yaml'));
-    // console.log(grammarData);
-    // Create a generator object from this data
-    const generator = new Improv(grammarData, {
-      filters: [Improv.filters.mismatchFilter(), Improv.filters.unmentioned(2)],
-      reincorporate: true
-    });
+const model = {};
+// Generate text and print it out
+var bird = generator.gen('root', model);
+console.log(bird);
 
-    const model = {};
-      var birds = [];
-       // Generate text and print it out
-       for(var i =0; i < 20; i++){
+//  console.log(model);
 
-       birds.push(generator.gen('root', model));
-       console.log(birds[i]);
-       console.log(" ");
-     }
-      //  console.log(model);
+//Creating Twitter App
+//Callback functions
+var error = function(err, response, body) {
+    console.log('ERROR [%s]', err);
+};
+var success = function(data) {
+    console.log('Data [%s]', data);
+};
+
+var Twitter = require('twitter-node-client').Twitter;
+
+
+// make a directory in the root folder of your project called data
+// copy the node_modules/twitter-node-client/twitter_config file over into data/twitter_config`
+// Open `data/twitter_config` and supply your applications `consumerKey`, 'consumerSecret', 'accessToken', 'accessTokenSecret', 'callBackUrl' to the appropriate fields in your data/twitter_config file
+
+var twitter = new Twitter();
+twitter.postTweet(bird, error, success);
