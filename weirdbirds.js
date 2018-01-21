f25f17f2db53d3ead498696bbecd669d6fe05878
dodechotomy <dodechotomy@gmail.com>
2018-01-21 16:47:10 -0500

    Fixed repetition issue in grammer

--- weirdbirds.js
+++ weirdbirds.js
@@ -7,30 +7,11 @@ const fs = require('fs-jetpack');
 const grammarData = yaml.load(fs.read('grammar.yaml'));
 // console.log(grammarData);
 
-  var dryish = function() {
-    var bonus = -1;
-
-    return function (group) {
-      if (!Array.isArray(group.tags)) return 0;
-      if (group.tags.length === 0) return 0;
-      var that = this;
-      var result = group.tags.find(function (t) {
-        // Return true if the tag is "novel".
-        var found = that.tagHistory.find(function (u) {
-          return u[0] === t[0];
-        });
-        return typeof found === 'undefined';
-      });
-      if (typeof result === 'undefined') return bonus;
-      return 0;
-    };
-  };
-
-
   // Create a generator object from this data
 const generator = new Improv(grammarData, {
-    filters: [Improv.filters.mismatchFilter(), Improv.filters.unmentioned(1), dryish],
-    reincorporate: true
+    filters: [Improv.filters.mismatchFilter(), Improv.filters.unmentioned()],
+    reincorporate: true,
+    binding: true
 });
 
 const model = {};
